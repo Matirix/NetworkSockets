33c64522d230d6461e0f2cfc962659fc5c1dfe7a
Matthew <Matirix@live.com>
2025-01-18 22:46:57 -0800

    added readfile from cmd line in client, and shift arg in server

--- client.c
+++ client.c
@@ -5,20 +5,62 @@
 #include <stdio.h>
 #include <sys/un.h>
 
+#define S_PATH "/tmp/socket"
 
+
+/**
+readFile - Reads the file and returns the contents
+
+@param file_name The file name
+Returns the contents
+*/
+char* readFile(char* file_name) {
+    FILE *file = fopen(file_name,"r");
+    if (file == NULL) {
+        perror("File Error");
+        exit(EXIT_FAILURE);
+    }
+    // GET SIZE
+    fseek(file, 0, SEEK_END); // Go to end
+    long file_size = ftell(file); // Get to the position to figure out size
+    fseek(file, 0, SEEK_SET); // Go back to the beginning
+
+    // Allocate a buffer to store the files
+    char* buffer = (char *) malloc(file_size + 1);
+    if (buffer) {
+        fread(buffer, 1, file_size, file);
+    }
+    fclose(file);
+    return buffer;
+}
+
+/**
+main - Entry point for the server socket
+
+@ argc Number of command line arguments
+@ argv an Array of Strings that represent the command line arguments.
+        i.e, arg[1] is used for a file name.
+*/
 int main(int argc, char*argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "USE: %s <filename> \n", argv[0]);
+        return 1;
+    }
+    const char *msg = readFile(argv[1]);
     struct sockaddr_un addr;
+    // CREATE SOCKET UNIX DOMAIN SOCKET
     int client_socket = socket(AF_UNIX, SOCK_STREAM,0);
     if (client_socket == -1) {
         perror("Socket Creation Error: ");
         return 1;
     }
 
+    // SET UP ADDRESS
     memset(&addr, 0, sizeof(addr));
     addr.sun_family = AF_UNIX;
-    strcpy(addr.sun_path, "/tmp/socket");
+    strcpy(addr.sun_path, S_PATH);
 
-    // Attempts to connect
+    // CONNECT TO SOCKET
     if (connect(client_socket,(struct sockaddr *) &addr, sizeof(addr)) == -1 ) {
         perror("Connection Error");
         close(client_socket);
@@ -27,24 +69,22 @@ int main(int argc, char*argv[]) {
 
     printf("Connected to Server\n");
 
-    const char *msg = "Hello, world!";
+
     if (send(client_socket, msg, strlen(msg),0) == -1) {
         perror("Sending Error");
+        close(client_socket);
         exit(EXIT_FAILURE);
     }
 
     char buffer[128];
-    ssize_t bytes = recv(client_socket, buffer, sizeof(buffer) - 1,0);
+    ssize_t bytes = recv(client_socket, buffer, sizeof(buffer) - 1, 0);
     if (bytes > 0) {
         // null terminator to signify end of string
         buffer[bytes] = '\0';
         printf("Recieved: %s\n", buffer);
     }
 
-
-
     close(client_socket);
-
-
+    return 0;
 
 }
--- server.c
+++ server.c
@@ -4,34 +4,76 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/un.h>
+#include <ctype.h>
 
-int BACKLOG = 5;
+#define BACKLOG 5
+#define S_PATH "/tmp/socket"
 
+
+
+/**
+* caeser_cipher_encrpyt - Encrypts by incrementing according to shift
+*
+* This ceaser cipher takes into account negatives, numbers exceeding 26.
+* @buffer: The string to be encrypted
+* @shift: Number of alphabetical shifts for each character
+* Return: Encrpyted string.
+*/
+char* caeser_cipher_encrypt(char buffer[], int shift) {
+    for (int i = 0; i < strlen(buffer); i++) {
+        if (isalpha(buffer[i])) {
+            if (islower(buffer[i])) {
+                buffer[i] = ((buffer[i] - 'a' + shift) % 26 + 26) % 26 + 'a';
+            } else if (isupper(buffer[i])) {
+                buffer[i] = ((buffer[i] - 'A' + shift) % 26 + 26) % 26 + 'A';
+            }
+        }
+        // Non-alphabetic characters remain unchanged
+    }
+    return buffer;
+}
+
+/**
+main - Entry point for the server socket
+
+@ argc Number of command line arguments
+@ argv an Array of Strings that represent the command line arguments.
+        i.e, arg[1] is used as an indicator of how many letters to shift for encryption.
+*/
 int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "USE: %s <shift> \n", argv[0]);
+        return 1;
+    }
+    int shift = atoi(argv[1]);
+
+    // SET UP SOCKET
     struct sockaddr_un addr;
     int server_socket = socket(AF_UNIX, SOCK_STREAM, 0);
     if (server_socket == -1) {
         perror("Socket Creation Error:");
-        return 1;
+        return -1;
     }
 
+    // BINDING
     // Setting addr parameters, IPC type and file location.
     memset(&addr, 0, sizeof(addr)); // Clears the memory by replacing all bytes to 0
     addr.sun_family = AF_UNIX; // Specifies that this will be used for IPC on same machine
     strcpy(addr.sun_path, "/tmp/socket"); // Dictates where the socket will be in the file system
 
     // Remove the socket file if it exists
-    if (access("/tmp/socket", F_OK) != -1) {
-        remove("/tmp/socket");
+    if (access(S_PATH, F_OK) != -1) {
+        remove(S_PATH);
     }
 
     // Associates the server socket with the addrs created above
     if (bind(server_socket, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
         perror("Binding Error");
         exit(EXIT_FAILURE);
+        return -1;
     }
 
-    // Begins listening mode
+    // LISTENING
     printf("Listening...\n");
     if (listen(server_socket, BACKLOG) == -1) {
         perror("Listen Error");
@@ -40,45 +82,37 @@ int main(int argc, char *argv[]) {
 
     printf("Server Socket is now accepting new connections\n");
 
-    // Client sockets
-    int c_socket;
-    struct sockaddr_un c_addr;
-    socklen_t c_addr_len = sizeof(c_addr);
-
-    while (1) {
-        // Accepting connection
-        c_socket = accept(server_socket, (struct sockaddr *)&c_addr, &c_addr_len);
-        if (c_socket == -1) {
-            perror("Accept Error");
-            continue; // Keep the server running even if one connection fails
-        }
-
-        // Recieving  code
-        printf("Accepted a new client connection\n");
-        char buffer[128];
-        ssize_t bytes = recv(c_socket, buffer, sizeof(buffer) - 1,0);
-        if (bytes > 0) {
-            // null terminator to signify end of string
-            buffer[bytes] = '\0';
-            printf("Recieved: %s\n", buffer);
-        }
-
-        // Response code here:
-        const char* msg = "Hello from Server";
-        if (send(c_socket, msg, strlen(msg), 0) == -1) {
-            perror("Server Response Error");
-            exit(EXIT_FAILURE);
-        }
-
-
-
-
-        // Close client socket after communication
-        close(c_socket);
+    // ACCEPT CONNECTION
+    int c_socket = accept(server_socket, NULL, NULL);
+    if (c_socket == -1) {
+        perror("Accept Error");
+        // continue; // Keep the server running even if one connection fails
     }
 
-    // Close the server socket when done
+    // RECIEVE MESSAGE
+    printf("Accepted a new client connection\n");
+    char buffer[128];
+    ssize_t bytes = recv(c_socket, buffer, sizeof(buffer) - 1,0);
+    if (bytes > 0) {
+        // null terminator to signify end of string
+        buffer[bytes] = '\0';
+        printf("Recieved: %s\n", buffer);
+    }
+
+    // SEND (RESPOND) WITH ENCRPYTED MESSAGE
+    char* encrypytedMessage = caeser_cipher_encrypt(buffer, shift);
+    if (send(c_socket, buffer, strlen(encrypytedMessage), 0) == -1) {
+        perror("Server Response Error");
+        exit(EXIT_FAILURE);
+    }
+    // CLEAN UP
+    close(c_socket);
     close(server_socket);
+    if (unlink(S_PATH) == -1) {
+        perror("Unlink Err");
+        exit(EXIT_FAILURE);
+    }
+
 
     return 0;
 }
